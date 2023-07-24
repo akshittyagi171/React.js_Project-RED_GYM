c3bf7a0cfc0e67ffd24f7e7adddb59623bd3728c
AKSHIT TYAGI <87603361+akshittyagi171@users.noreply.github.com>
2023-07-24 21:10:27 +0530

    Add files via upload

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      'X-RapidAPI-Key': 'e99bab7531mshc4c64cfc88010c7p1d9a4djsn48dfdf74da45',
+    },
+  };
+  
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': 'e99bab7531mshc4c64cfc88010c7p1d9a4djsn48dfdf74da45',
+    },
+  };
+  
+  export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+  
+    return data;
+  };
